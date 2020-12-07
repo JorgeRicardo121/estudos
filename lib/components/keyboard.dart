4ae45c2816873d1894fbaa7caf3bbce231072bca
Estação_Jorge <ricardo10caldas@gmail.com>
2020-12-07 12:33:10 -0300

    Ajustes na orientação / Espaçamento de botoes

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -18,24 +18,28 @@ class Keyboard extends StatelessWidget {
             Buttom(text: '%', color: Buttom.DARK, cb: cb),
             Buttom.operation(text: '/', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Buttom(text: '7', cb: cb),
             Buttom(text: '8', cb: cb),
             Buttom(text: '9', cb: cb),
             Buttom.operation(text: '*', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Buttom(text: '4', cb: cb),
             Buttom(text: '5', cb: cb),
             Buttom(text: '6', cb: cb),
             Buttom.operation(text: '-', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Buttom(text: '1', cb: cb),
             Buttom(text: '2', cb: cb),
             Buttom(text: '3', cb: cb),
             Buttom.operation(text: '+', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Buttom.big(text: '0', cb: cb),
             Buttom(text: ',', cb: cb),
